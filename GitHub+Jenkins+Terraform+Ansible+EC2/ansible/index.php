e27bf989b55da61b16c4c952379007fa323e7e32
LNR <97195796+Loicnka@users.noreply.github.com>
2025-05-09 03:38:23 +0200

    index.php

--- GitHub+Jenkins+Terraform+Ansible+EC2/ansible/index.php
+++ GitHub+Jenkins+Terraform+Ansible+EC2/ansible/index.php
@@ -1,1 +1,8 @@
+<body bgcolor='white'>
+<pre>
+<?php
+print("<b><font color=\"blue\">!! Hello !! This is Euran,\n\n! Welcome ! to My Web Server</font>\n\n");
+print (`/usr/sbin/ifconfig`);
+?>
+</pre>
 
